ab7e63bb87fb3421e7ccbf1ddb606ece829e6ea2
Christopher Millan <chrmil11@gmail.com>
2019-04-07 17:11:38 -0500

    Add cleaning function to remove whitespace

--- assets/js/site.js
+++ assets/js/site.js
@@ -7,4 +7,34 @@
     return;
   }
 
+  // Value cleaning functions
+
+  //  Payment form inputs:
+  //    TODO: Full name (non-empty string?)
+  //    TODO: Credit Card Number (16-digits, remove all whitespace, no other chars)
+  //    TODO: Expiration date (month [dropdown value] + year [number])
+  //    TODO: Security code (3 or 4 digit number code)
+  //    TODO: Zip code (5# or 5#-4# number code)
+  //    TODO: Email addresss (Remove whitespace, includes @)
+
+
+  function remove_whitespace(value) {
+    // Remove excess whitespace
+    value = value.replace(/^ +/g, '');  // Remove whitespace from beginning
+    value = value.replace(/ +$/g, '');  // Remove whitespace from end
+    value = value.replace(/ +/g, ' ');  // Remove extra whitespace in the middle
+    return value;
+  }
+
+  // Validation functions
+
+  // Event Listeners
+  // TODO: Enable/disable submit button if all fields are valid
+  // ETC
+
+  //  TODO: Update expiration month/year based on current date
+  //   ie: if current mo/yr is june 2020, have the mo initialize to 06 and year to 2020, with min 2020 and max 2040 (+20)
+  //  TODO: Warnings/errors for missing inputs
+  //   ie: check if any fields are empty or contain errors, live feedback for invalid input
+
 });
