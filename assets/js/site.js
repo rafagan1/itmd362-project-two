663103c05a2d0f293ecc26b085cf67152ac20918
Christopher Millan <chrmil11@gmail.com>
2019-04-07 10:08:41 -0500

    Add browser checker for event listener

---
+++ assets/js/site.js
@@ -0,0 +1,10 @@
+"use strict";
+
+(function () {
+  // Browser check
+  if (!('querySelector' in document && 'addEventListener' in document)) {
+    console.log('querySelector or addEventListener not available');
+    return;
+  }
+
+});
